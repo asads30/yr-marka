b797547b3b5a88009caf134a9dce66e1735c7e82
asads30 <itmasteruz@yandex.com>
2023-09-27 20:07:30 +0500

    01

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ import vuetify from 'vite-plugin-vuetify'
 export default defineConfig({
   server: {
     proxy: {
-      '/admin/': 'https://wpaka.site/',
+      '/admin/': 'http://94.241.175.180:3000/',
     }
   },
   plugins: [
